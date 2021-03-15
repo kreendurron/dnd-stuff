14b629f89a4f823c71de42afe15a034dda8415da
Nicholas E. Brown <kreendurron@gmail.com>
2021-03-15 15:43:11 -0500

    quick push

--- aliases/miningtown.py
+++ aliases/miningtown.py
@@ -1,4 +1,4 @@
-embed
+!tembed
 <drac2>
 
 towns = load_json(get_gvar("fa6f4eb9-1ea4-4c76-b6b8-eeed4ee3a0ed"))
@@ -7,12 +7,16 @@ town = towns[0]
 taxablePopulation = float(town.population) - float(town.laborers)
 income = float(town.workdays) * float(town.laborProfitability) * float(town.laborers)
 taxRevenue = float(town.taxIncome)*float(income)
-expenses = float(town.miscExpenses) + float(town.laborPayrate)
-nationalTax = float(town.taxExpense)*100
+
+miscExpenseRate = float(town.miscExpenses)*100
+laborExpense = float(town.laborPayrate)*float(town.laborers)
+expenses = miscExpenseRate + laborExpense
 
 
-grossRevenue = float(income) + float(taxRevenue)
-netProfit = float(grossRevenue) - float(expenses) * float(nationalTax)
+grossRevenue = floor(income + taxRevenue)
+netProfit = floor(float(grossRevenue) * float(town.taxExpense) - float(expenses))
+dailyIncome = floor(netProfit/365)
+
 
 </drac2>
 
@@ -34,13 +38,13 @@ taxRevenue: {{taxRevenue}}
 Expenses:
 National Tax: {{round(float(town.taxExpense)*100)}}%
 Miscelaneous Expenses: {{round(float(town.miscExpenses)*100)}}%
-Labor Pay: {{town.laborPayrate}} gp/anum (2sp per laborer per day *300 days)
+Labor Pay: {{town.laborPayrate}} GP/anum
 "
 
 -f "
-Gross Revenue: {{f'{grossRevenue:,}'}} per anum
-Net Profit: {{f'{netProfit:,}'}} per anum (still working on calc)
-
+Gross Revenue: {{f'{grossRevenue:,}'}} GP/anum
+Net Profit: {{f'{netProfit:,}'}} GP/anum
+Daily Income: {{f'{dailyIncome:,}'}} GP/day
 "
 
 -footer "You can set the tax rate for your town. and other options coming soooon. (work in progress)"
