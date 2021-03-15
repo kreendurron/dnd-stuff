3487129813f1aab96a969d0a27fd69ee5ddcddff
Nicholas E. Brown <kreendurron@gmail.com>
2021-03-15 16:00:56 -0500

    Formatting and code cleanup

--- aliases/miningtown.py
+++ aliases/miningtown.py
@@ -4,17 +4,21 @@
 towns = load_json(get_gvar("fa6f4eb9-1ea4-4c76-b6b8-eeed4ee3a0ed"))
 town = towns[0]
 
+townTaxRate = float(town.taxIncome)*100
 taxablePopulation = float(town.population) - float(town.laborers)
 income = float(town.workdays) * float(town.laborProfitability) * float(town.laborers)
-taxRevenue = float(town.taxIncome)*float(income)
+townTaxRevenue = floor(float(town.taxIncome)*income)
+
 
 miscExpenseRate = float(town.miscExpenses)*100
 laborExpense = float(town.laborPayrate)*float(town.laborers)
 expenses = miscExpenseRate + laborExpense
 
 
-grossRevenue = floor(income + taxRevenue)
-netProfit = floor(float(grossRevenue) * float(town.taxExpense) - float(expenses))
+grossRevenue = floor(income + townTaxRevenue)
+nationalTaxRate = float(town.taxExpense)*100
+nationalTaxExpense = floor(float(town.taxExpense)*grossRevenue)
+netProfit = floor(grossRevenue * float(town.taxExpense) - expenses)
 dailyIncome = floor(netProfit/365)
 
 
@@ -26,22 +30,24 @@ dailyIncome = floor(netProfit/365)
 
 -f
 "
-Town Statistics:
+**Town Statistics:**
 Population: {{town.population}}
 Laborers: {{town.laborers}}
-Tax Rate: {{round(float(town.taxIncome)*100)}}%
-taxRevenue: {{taxRevenue}}
+Town Tax Rate: {{round(float(townTaxRate))}}%
+Town Tax Revenue: {{f'{townTaxRevenue:,}'}} GP/anum}
 "
 
 -f
 "
-Expenses:
-National Tax: {{round(float(town.taxExpense)*100)}}%
-Miscelaneous Expenses: {{round(float(town.miscExpenses)*100)}}%
+**Expenses:**
+National Tax Rate: {{round(nationalTaxRate)}}%
+National Tax Expense: {{f'{nationalTaxExpense:,}'}} GP/anum}
+Miscelaneous Expenses: {{round(miscExpenseRate)}}%
 Labor Pay: {{town.laborPayrate}} GP/anum
 "
 
 -f "
+**Earnings**
 Gross Revenue: {{f'{grossRevenue:,}'}} GP/anum
 Net Profit: {{f'{netProfit:,}'}} GP/anum
 Daily Income: {{f'{dailyIncome:,}'}} GP/day
