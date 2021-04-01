90537e8062a09f4a85dbfe76979db334d17be718
Nicholas E. Brown <kreendurron@gmail.com>
2021-04-01 15:03:37 -0500

    sync

--- aliases/town.py
+++ aliases/town.py
@@ -1,11 +1,13 @@
-embed
+!tembed
 <drac2>
 out = []
 
 g = load_json(get_gvar("575c8d91-53f2-4b44-a73c-ead0d9e8a1e5"))
+income = float(g[townID]["Laborers"]) * float(g[townID]["Labor Payrate"])#laborers * laborPayrate (425*60=25500)
+expenses = float(g[townID]["National Tax Rate"]) * float(income) #income * national taxrate + income
 if get("townID") in g.keys():
-    grossRevenue = 1000
-    netProfit = 0
+    grossRevenue = 0 #laborers * laborOutput * workdays + taxrate on taxablePopulation
+    netProfit = 0 #grossRevenue - expenses
     dailyIncome = 0
     out.append(f"""
 -title "{name}'s Town: {townID}"
@@ -25,12 +27,79 @@ return ' '.join(out)
 </drac2>
 
 -f "
+{{typeof(g[townID]["National Tax Rate"])}}
 Earnings
-Gross Revenue: {{f'{grossRevenue:,}'}} GP/anum
-Net Profit: {{f'{netProfit:,}'}} GP/anum
-Daily Income: {{f'{dailyIncome:,}'}} GP/day
+Income: {{f'{income:,}'}} GP/anum
+Expenses: {{f'{expenses:,}'}} GP/anum
+Gross Revenue: {{f'{grossRevenue:,}'}} GP/anum (not calculated yet)
+Net Profit: {{f'{netProfit:,}'}} GP/anum (not calculated yet)
+Daily Income: {{f'{dailyIncome:,}'}} GP/day (not calculated yet)
 "
 
 -f """You can set the tax rate for your town and other options coming soooon.
 Feel free to help contribute @ [mygithub](https://github.com/kreendurron/dnd-stuff 'https://github.com/kreendurron/dnd-stuff')"""
 -footer "Work In Progress"
+
+
+
+
+
+
+```py
+!tembed
+<drac2>
+out = []
+
+g = load_json(get_gvar("575c8d91-53f2-4b44-a73c-ead0d9e8a1e5"))
+income = float(g[townID]["Laborers"]) * float(g[townID]["Labor Payrate"])#laborers(425) * laborPayrate(60) (425*60=25500)
+expenses = float(float(g[townID]["National Tax Rate"])/100 * float(income)) #income(25000) * national taxrate(75%) + income
+
+if get("townID") in g.keys():
+    out.append(f"""
+-title "{name}'s Town: {townID}"
+-f "**Statistics**"
+""")
+    for k,v in g[townID].items():
+        out.append(f''' -f "{k.title()}|{v}|inline" ''')
+
+else:
+    out.append(f"""-title "It doesn't look like you've founded a town yet!" -desc "Que The Sad Trombone Sound." """)
+
+return ' '.join(out)
+
+</drac2>
+
+-f "
+Earnings
+Income: {{f'{income:,}'}} GP/anum
+Expenses: {{f'{expenses:,}'}} GP/anum
+
+"
+```
+
+
+
+
+
+!tembed
+<drac2>
+out = []
+
+g = load_json(get_gvar("575c8d91-53f2-4b44-a73c-ead0d9e8a1e5"))
+income = float(g[townID]["Laborers"]) * float(g[townID]["Labor Payrate"])
+nationalTaxRate = float(float(g[townID]["National Tax Rate"]))#laborers(425) * laborPayrate(60) - ()
+um1 = nationalTaxRate * income
+um2 = nationalTaxRate * income / 100
+</drac2>
+
+-f "
+inspecting:
+nationalTaxRate * income = {{um1}}
+nationalTaxRate * income / 100 = {{um2}}
+national tax rate = {{g[townID]["National Tax Rate"]}}
+typeof nationalTaxRate = {{typeof(g[townID]["National Tax Rate"])}}
+qty of laboreres = {{g[townID]["Laborers"]}}
+labor Payrate = {{g[townID]["Labor Payrate"]}}
+Income: {{f'{income:,}'}} GP/anum
+
+"
